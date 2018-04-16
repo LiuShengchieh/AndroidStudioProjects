70a3d6d8f378f428192ee5dc0560e517bd96e27f
liushengjie <a140@140deMacBook-Air.local>
2018-04-16 13:14:39 +0800

    知识页完成

---
+++ CivilServant/app/src/main/java/com/example/a140/civilservant/ui/SecretActivity.java
@@ -0,0 +1,20 @@
+package com.example.a140.civilservant.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.a140.civilservant.R;
+
+/**
+ * Created by a140 on 2018/4/16.
+ * 国考揭秘
+ */
+
+public class SecretActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secret);
+    }
+}
